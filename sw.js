5befcd2e534ae457d91474c1b7a8846665c4e5ee
antigones <juna.salviati@gmail.com>
2025-09-06 14:44:03 +0200

    First commit

---
+++ sw.js
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/index-B4Mnpjs9.css",
+    "revision": null
+  }, {
+    "url": "assets/index-DpJFpdSw.js",
+    "revision": null
+  }, {
+    "url": "assets/primeicons-Dr5RGzOO.svg",
+    "revision": null
+  }, {
+    "url": "favicon.svg",
+    "revision": "71dcfd191507c31dc79efe3341dfa3b9"
+  }, {
+    "url": "index.html",
+    "revision": "366ab3cc570f2bbe8b0db8f391d09a7b"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "2fd3049abb1967a2a41dd145dab0ae3c"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "f24c9384006bbc8de95ed69990459dca"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "4db5b8fe442a8f8fdc6e35cd40138057"
+  }, {
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "vite.svg",
+    "revision": "8e3a10e157f75ada21ab742c022d5430"
+  }, {
+    "url": "favicon.svg",
+    "revision": "71dcfd191507c31dc79efe3341dfa3b9"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "f24c9384006bbc8de95ed69990459dca"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "4db5b8fe442a8f8fdc6e35cd40138057"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "2fd3049abb1967a2a41dd145dab0ae3c"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
